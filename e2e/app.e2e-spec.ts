9f277c99cb26e302c607cf293fc28d38f1bec42f
chuan <chuan@tenthousandcoffees.com>
2017-08-03 00:01:57 -0400

    Use webpack & eslint & autoformat code

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Angular4DemoPage } from './app.po';
 
 describe('angular4-demo App', () => {
-  let page: Angular4DemoPage;
+    let page: Angular4DemoPage;
 
-  beforeEach(() => {
-    page = new Angular4DemoPage();
-  });
+    beforeEach(() => {
+        page = new Angular4DemoPage();
+    });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
+    it('should display welcome message', () => {
+        page.navigateTo();
+        expect(page.getParagraphText()).toEqual('Welcome to app!');
+    });
 });
