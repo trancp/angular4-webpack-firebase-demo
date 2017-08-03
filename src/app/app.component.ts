c935eded86aeb104100d316f28beb933285888f2
chuan <chuan@tenthousandcoffees.com>
2017-08-03 01:52:38 -0400

    Integrate firebase & angularfire2 into project
    - import FormsModule to support ngModel on Input elements
    - import HttpModule to support pipe async in templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
     selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'app';
+    items: FirebaseListObservable<any[]>;
+    msgVal: string;
+
+    constructor(public angularFireDatabase: AngularFireDatabase) {
+        this.items = angularFireDatabase.list('/messages', {
+            query: {
+                limitToLast: 50
+            }
+        });
+    }
+
+    Send(desc: string) {
+        this.items.push({ message: desc });
+        this.msgVal = '';
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: 'AIzaSyDEKFhJ1Nf8-RHXT39ryGJYKzEzbnrWKNA',
+    authDomain: 'angular4-demo-c5da4.firebaseapp.com',
+    databaseURL: 'https://angular4-demo-c5da4.firebaseio.com',
+    projectId: 'angular4-demo-c5da4',
+    storageBucket: 'angular4-demo-c5da4.appspot.com',
+    messagingSenderId: '798909163850'
+};
 
 @NgModule({
     declarations: [
         AppComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFireDatabaseModule
     ],
     providers: [],
     bootstrap: [AppComponent]
